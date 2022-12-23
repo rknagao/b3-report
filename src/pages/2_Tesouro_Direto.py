parcial solution to kpi showing incorrect value

src/pages/2_Tesouro_Direto.py
@@ -2,10 +2,12 @@ import altair as alt
 import datetime
 import dateutil
 import json
-import pandas as pd
-import os
-import streamlit as st
 import local_lib as lib
+from millify import millify
+import numpy as np
+import os
+import pandas as pd
+import streamlit as st
 import yfinance as yf
 
 ##########################
@@ -74,13 +76,61 @@ else:
         Principais números da carteira.
         ''')
 
-        from dateutil.relativedelta import relativedelta 
         date_interval = st.slider('Selecione o intervalo',
                                   value=(df_tesouro_historico['data'].min().to_pydatetime(), df_tesouro_historico['data'].max().to_pydatetime()),
                                   min_value=df_tesouro_historico['data'].min().to_pydatetime(),
                                   max_value=df_tesouro_historico['data'].max().to_pydatetime(),
                                   step=datetime.timedelta(days=90),
                                   format='YYYY-MM-DD')
+        
+        df_date = pd.DataFrame({'data': pd.date_range(df_tesouro_historico['data'].min().to_pydatetime(), df_tesouro_historico['data'].max().to_pydatetime())})
+        df_kpi = pd.merge(df_date, df_tesouro_historico, on='data', how='left')
+        df_kpi = df_kpi.groupby('data').agg({'qt':'sum', 'qt_acum':'sum',  'vl_atualizado':'sum'}).reset_index()
+        df_kpi['vl_atualizado'] = np.where(df_kpi['qt_acum'] != 0, df_kpi['vl_atualizado'], np.nan)
+        df_kpi.fillna(method='ffill', inplace=True)
 
-        st.write(f"Data inicio: {date_interval[0]}")
-        st.write(f"Data fim: {date_interval[1]}")
+        st.dataframe(df_kpi.loc[df_kpi['data'] == date_interval[0]])
+        st.dataframe(df_kpi.loc[df_kpi['data'] == date_interval[1]])
+
+        # Cálculo dos aportes históricos.
+        vl_aporte = df_kpi.loc[(df_kpi['qt'] != 0) &
+                               (df_kpi['vl_total'] > 0) &
+                               (df_kpi['data'] <= date_interval[1]), 'vl_total'].sum()
+
+        vl_aporte_delta = df_kpi.loc[(df_kpi['qt'] != 0) &
+                                     (df_kpi['vl_total'] > 0) &
+                                     (df_kpi['data'].between(date_interval[0], date_interval[1])), 'vl_total'].sum()
+
+        # Cálculo dos valores resgatados.
+        vl_resgate = df_kpi.loc[(df_kpi['qt'] != 0) &
+                                (df_kpi['vl_total'] < 0) &
+                                (df_kpi['data'] <= date_interval[1]), 'vl_total'].sum()
+
+        vl_resgate_delta = df_kpi.loc[(df_kpi['qt'] != 0) &
+                                      (df_kpi['vl_total'] < 0) &
+                                      (df_kpi['data'].between(date_interval[0], date_interval[1])), 'vl_total'].sum()
+
+        # Cálculo do valor patrimonial.
+        vl_patrimonio = df_kpi.loc[df_kpi['data'] == date_interval[1], 'vl_atualizado'].sum()
+        vl_patrimonio_delta = vl_patrimonio - df_kpi.loc[df_kpi['data'] == date_interval[0], 'vl_atualizado'].sum()
+
+        # Cálculo do rendimento.
+        #vl_resgate = df_tesouro_historico.loc[(df_tesouro_historico['qt'] != 0) & (df_tesouro_historico['vl_total'] < 0), 'vl_total'].sum() * -1
+        #vl_investido = df_tesouro_historico.loc[df_tesouro_historico['data']==df_tesouro_historico['data'].max(), 'vl_atualizado'].sum()
+
+        rendimento_nominal = round((vl_patrimonio - vl_resgate - vl_aporte) / vl_aporte * 100 , 1)
+        rendimento_nominal_delta = round((vl_patrimonio_delta - vl_resgate_delta - vl_aporte_delta) / vl_aporte_delta * 100 , 1)
+        #rendimento_anual = (1 + rendimento_nominal) ^ (12/34) - 1 
+        
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.metric(label="Aporte", value=f"R$ {millify(vl_aporte)}", delta=f"R$ {millify(vl_aporte_delta)}")
+
+        with col2:
+            st.metric(label="Resgate", value=f"R$ {millify(vl_resgate)}", delta=f"R$ {millify(vl_resgate_delta)}", delta_color='inverse')
+
+        with col3:
+            st.metric(label="Patrimônio", value=f"R$ {millify(vl_patrimonio)}", delta=f"R$ {millify(vl_patrimonio_delta)}")
+
+        with col4:
+            st.metric(label="Rendimento", value=f"{rendimento_nominal} %", delta=f"{rendimento_nominal_delta} %")